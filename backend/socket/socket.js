3157d0c18181582991882f1d885252c69b535831
mishra06 <mishra.vibhakar7262@gmail.com>
2024-07-09 13:27:37 +0530

    cors

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000','https://mern-project-psi-jet.vercel.app/'],
+        origin:['http://localhost:3000','https://mern-project-psi-jet.vercel.app'],
         methods:['GET', 'POST'],
     },
 });
